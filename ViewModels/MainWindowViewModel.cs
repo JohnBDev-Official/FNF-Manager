355887ca86990957fc5a6c14edf4b39fc6ef9443
johnb17 <johnb@johnbdev.net>
2026-06-23 12:06:37 -0400

    Started building settings model. Started theming slightly, the theming is pretty much just in concept stages. The theming plan is fnf style here in Avalonia.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,20 @@
-﻿namespace FNF_Manager.ViewModels
+﻿using Avalonia;
+using Avalonia.Styling;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FNF_Manager.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        //public string Greeting { get; } = "Welcome to FNF Manager!";
+        [ObservableProperty]
+        private bool isDarkMode;
+
+        partial void OnIsDarkModeChanged(bool value)
+        {
+            if (Application.Current is { } app)
+            {
+                app.RequestedThemeVariant = value ? ThemeVariant.Dark : ThemeVariant.Light;
+            }
+        }
     }
 }
