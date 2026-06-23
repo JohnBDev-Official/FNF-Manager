0228cc39920458091aab23f925d357a6f002759d
johnb17 <johnb@johnbdev.net>
2026-06-23 11:27:32 -0400

    Started building the ModInfo model.

---
+++ Models/ModInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FNF_Manager.Models
+{
+    /// <summary>
+    /// Represents a Friday Night Funkin' mod installed or tracked by the manager.
+    /// </summary>
+    public class ModInfo
+    {
+        /// <summary>
+        /// The display name of the mod.
+        /// </summary>
+        public string Name { get; set; } = "";
+
+        /// <summary>
+        /// The folder name of the mod in the FNF Manager mods directory
+        /// </summary>
+        public string FolderName { get; set; } = "";
+
+        /// <summary>
+        /// The version number of the mod.
+        /// </summary>
+        public string Version { get; set; } = "";
+
+        /// <summary>
+        /// Where the mod is from, used when checking and pulling new versions of the mod.
+        /// </summary>
+        public enum ModSource
+        {
+            GameBanana,
+            LocalZip,
+            ManualFolder,
+            Unknown
+        }
+
+        /// <summary>
+        /// If the mod is actually installed or in the user's list of mods.
+        /// </summary>
+        public bool IsInstalled { get; set; }
+}
+}
