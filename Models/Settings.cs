77d5d966dd1fadd934f54762a5437d8a371d7403
johnb17 <johnb@johnbdev.net>
2026-06-23 21:46:39 -0400

    Cleaned up the settings page, still need the settings to store somewhere though.

--- Models/Settings.cs
+++ Models/Settings.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace FNF_Manager.Models
 {
-    internal class Settings
+    public class Settings
     {
+        /// <summary>
+        /// Where to store FNF base game
+        /// </summary>
+        public string BaseGameDir { get; set; } = "";
+        /// <summary>
+        /// Where to store FNF mods
+        /// </summary>
+        public string ModsDir { get; set; } = "";
+        /// <summary>
+        /// Where to store FNF save files
+        /// </summary>
+        public string SavesDir { get; set; } = "";
     }
 }
